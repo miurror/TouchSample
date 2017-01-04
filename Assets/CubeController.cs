07e6e982aceddd05f83e48feb334612da5a71595
miurror <miurror.jp@gmail.com>
2017-01-04 21:47:53 +0900

    Add CubeController.cs

---
+++ Assets/CubeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Hello Unity.");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
