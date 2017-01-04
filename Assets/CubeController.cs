33811a1039623f24c3b674208ce1a248cde8d6b2
miurror <miurror.jp@gmail.com>
2017-01-04 23:20:20 +0900

    Add Lance/Bullet-like behavior via X-Box controller to CubeController.cs

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		//Debug.Log("Hello Unity.");	
-	}
+	public float speed = 0f;
+	public float bulletSpeed = 0f;
+	public bool bulletMode = false;
 	
-	// Update is called once per frame
 	void Update () {
-		transform.Translate(0f,0f,0.1f);
+
+		if(Input.GetButtonUp("Fire1")){
+			bulletMode = true;
+		}
+
+		float v1 = Input.GetAxis("Horizontal") * speed;
+		float v2 = Input.GetAxis("Vertical") * speed;
+		float v3 = bulletMode ? bulletSpeed : 0f;
+		float r1 = Input.GetAxis("Horizontal2");
+		float r2 = Input.GetAxis("Vertical2");
+
+		transform.Translate(v1,v2,v3);
+		transform.Rotate(r2,r1,0f);
+
 	}
 }
