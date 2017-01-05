daea2e4a7af836829bd5d8147d3298d570bd14f4
miurror <miurror.jp@gmail.com>
2017-01-05 13:56:54 +0900

    Add EnemyBox and EnemyBoxScript.cs

---
+++ Assets/Scripts/EnemyBoxScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBoxScript : MonoBehaviour {
+
+	public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+		for(int i=1; i<=10; ++i){
+			Instantiate(obj, new Vector3(Random.Range(-5f,5f),Random.Range(-5f,5f),Random.Range(-5f,5f)), Quaternion.identity);
+		}
+	}
+	
+}
