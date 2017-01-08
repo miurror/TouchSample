5d801712b66ee1cdcd957adabc5e22067a4c32fc
miurror <miurror.jp@gmail.com>
2017-01-09 02:55:16 +0900

    Add hands

---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour {
+
+	public OVRInput.Controller controller;
+	void Update () {
+		transform.localPosition = OVRInput.GetLocalControllerPosition(controller);	
+		transform.localRotation = OVRInput.GetLocalControllerRotation(controller);	
+	}
+}
